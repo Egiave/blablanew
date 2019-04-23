7e70c75e33f970e08d9dcc41ebfc9269b9a2f9f1
EV <e.veblauskaite@gmail.com>
2019-04-23 13:56:00 +0300

    pridetas index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHPEASTER</title>
+        <style type="text/css">
+            body {
+                background-image: url('https://i.gifer.com/AcH.mp4');
+                background-size: cover;
+                height: 100vh;
+            }
+           
+        </style>
+    </head>
+    <body>
+        <h1>
+            We are COSMIC EASTER
+        </h1>
+    </body>
+</html>
